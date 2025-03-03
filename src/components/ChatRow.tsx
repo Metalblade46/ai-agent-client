051af2dd98be1529db8e00665c856b7a834651aa
Sabbyasachi Chakraborty <chakrabortysabbyasachi@gmail.com>
2025-03-04 01:45:08 +0530

    made changes to configurations and added last chat visibility in chats

--- src/components/ChatRow.tsx
+++ src/components/ChatRow.tsx
@@ -4,11 +4,16 @@ import { usePathname, useRouter } from 'next/navigation'
 import { NavigationContext } from '@/lib/NavigationProvider';
 import { TrashIcon } from '@radix-ui/react-icons';
 import { cn } from '@/lib/utils';
+import { useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
 
 const ChatRow = ({chat,onDelete}: {chat: Doc<"chats">, onDelete: (id: Id<"chats">) => Promise<void>}) => {
     const router = useRouter();
     const pathName = usePathname()
     const {closeMobileNav} = use(NavigationContext)
+    const lastMessage = useQuery(api.messages.getLastMessage,{
+        chatId: chat._id
+    })
     const handleclick = ()=>{
         router.push(`/dashboard/chats/${chat._id}`)
         closeMobileNav()
@@ -25,10 +30,13 @@ const ChatRow = ({chat,onDelete}: {chat: Doc<"chats">, onDelete: (id: Id<"chats"
 
   return (
     <div className={cn('group w-full border border-gray-200/30 mx-1 p-3 shadow-lg rounded-lg cursor-pointer',pathName.includes(chat._id)? 'bg-blue-600 text-white': 'bg-white hover:bg-blue-200/50 text-gray-600')} onClick={handleclick}>
-        <div className='flex justify-between items-center text-sm'>{chat.title}
+        <div className='flex justify-between items-center text-sm'>
+            {
+                lastMessage ? `${lastMessage.role=="user"? "You: ": "AI: "} ${lastMessage.content.replace(/\\n/g, "\n").substring(0,25)}...`: <span className="text-gray-400">New conversation</span>
+            }
         <TrashIcon className='text-transparent group-hover:text-red-400 hover:scale-150 transition-all ease-in duration-200 cursor-pointer' onClick={handleDelete}/>
         </div>
-        <div className='text-xs mt-2 opacity-60'>{getTimeDiff(new Date(chat.createdAt),new Date())}</div>
+        <div className='text-xs mt-2 opacity-60'>{getTimeDiff(new Date(lastMessage?.createdAt || chat.createdAt),new Date())}</div>
     </div>
   )
 }
